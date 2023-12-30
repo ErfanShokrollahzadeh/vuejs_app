9a271260d00be6f973935d28f0fe2a7ff5af63d0
ERFAN <erfan.shokr300@gmail.com>
2023-12-30 19:14:21 +0330

    this section I use component method as beginner level

--- custom3.js
+++ custom3.js
@@ -1,8 +1,27 @@
 let app1
+let app2
+const data = {title: 'Hello Vue!'}
+
+
+const component = {
+    data: function () {
+        return data;
+    },
+    methods: {
+        changetitle: function () {
+            this.title = 'change title';
+        }
+    },
+    template: '<div> <p>this is title: {{title}}</p> <br> <button v-on:click="changetitle">change title</button> </div>'
+};
 
 app1 = new Vue({
     el: '#app1',
-    data: {
-        title: 'Hello Vue!'
+    components:{
+        'my-component': component
     }
+});
+
+app2 = new Vue({
+    el: '#app2',
 });
